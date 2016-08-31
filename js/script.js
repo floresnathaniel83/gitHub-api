48108ad0945ad356467f9b1c675f61e56540349c
Nathaniel Flores <nathanielflores@Nathaniels-MacBook-Pro.local>
2016-08-30 22:23:53 -0500

    add features and styles

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-console.log("hello world")
 
 try { 
 	var token = GLOBAL_TOKEN
@@ -7,67 +6,91 @@ catch (e) {
 	var token = ''
 }
 
+var myUserUrl = 'https://api.github.com/users/floresnathaniel83'
+var myRepoUrl = 'https://api.github.com/users/floresnathaniel83/repos'
+var params = {
+	access_token: token
 
-//https://api.github.com/users/floresnathaniel83
-//https://api.github.com/users/floresnathaniel83/repos
+}
 
-var promiseProfileUrlRoot = "https://api.github.com/users/",
-	repoUrlRoot = "https://api.github.com/users/",
-	profileParams = {
-		//login: "floresnathaniel83",
-		// name: "Nathaniel Flores",
-		// blog: null,
-		// location: "Houston, TX",
-		// email: "floresnathaniel83@gmail.com",
-		// avatar_url: "https://avatars.githubusercontent.com/u/18020008?v=3",
-		// html_url: "https://github.com/floresnathaniel83",
-		// created_at: "2016-03-23T02:21:11Z",
-		// followers: 0,
-		// following: 0
-	}
+var genParamString = function(paramObject) {
+ 	if(token !== " ") {
+	   	var pString = "?"
+	    	for (var key in paramObject) {
+	    		pString += key + "=" + paramObject[key] + "&" 
+	     	}
 
-	repoParams = {
-	}
+	     	return pString.substr(0,pString.length - 1)
 
-	if (token) {
-		profileParams.access_token = token
-		repoParams.access_token = token
-	}
+ 		} else {
 
+ 			return token
+
+ 		} 
+}
+
+var myFullUserUrl = myUserUrl + genParamString(params)
+var myFullRepoUrl = myRepoUrl + genParamString(params)
 
 var gitHubQuery = function(loginInput) {
 
-	// profileParams.login = loginInput
+	var promiseProfileUrlRoot = "https://api.github.com/users/"
 
-	var promiseProfileUrlFull = promiseProfileUrlRoot + loginInput + genParamString(profileParams)
-	console.log(promiseProfileUrlFull)
-	var repoUrlFull = repoUrlRoot + loginInput + "/repos" + genParamString(repoParams)
-	console.log(repoUrlFull)
+	var promiseProfileUrlFull = promiseProfileUrlRoot + loginInput + genParamString(params)
+	var repoUrlFull = promiseProfileUrlRoot + loginInput + "/repos" + genParamString(params)
 
-	var profilePromise = $.getJSON(promiseProfileUrlFull)
-	var repoPromise = $.getJSON(repoUrlFull)
+	var searchProfilePromise = $.getJSON(promiseProfileUrlFull)
+	var searchRepoPromise = $.getJSON(repoUrlFull)
 
-	profilePromise.then(profileDataHandler)
-	repoPromise.then(repoDataHandler)
+	searchProfilePromise.then(profileDataHandler)
+	searchRepoPromise.then(repoDataHandler)
 
 }
 
+var inputNode = document.querySelector('input[type="text"]')
 
-var genParamString = function(paramObject) {
- 	
-   	var pString = "?"
-    for (var key in paramObject) {
-    	// if we're looking at the access_token key and its value is empty string,
-    	// // skip this iteration
-    	// if (key === "access_token" && !paramObject[key]) {
-    	// 	continue
-    	// }
-    	pString += key + "=" + paramObject[key] + "&" 
-     }
-     return pString.substr(0,pString.length - 1) 
-
-   
+var handleGitHubSearch = function (eventObj) {
+	if(eventObj.keyCode === 13) {
+		var inputElement = eventObj.target
+		var inputValue = inputElement.value
+		gitHubQuery(inputValue)
+		inputElement.value = ""
+	}
 }
+
+inputNode.addEventListener('keydown', handleGitHubSearch)
+
+var updatedLast = function(repoUpdateTime) {
+
+	var timeNow = new Date()
+	var timeThen = new Date(repoUpdateTime)
+	var timeDiff = timeNow - timeThen
+    
+    var seconds = Math.floor(timeDiff/1000) + " second"
+    var minutes = Math.floor(timeDiff/(1000 * 60)) + " minute"
+    var hours = Math.floor(timeDiff/(1000 * 60 * 60)) + " hour"
+    var days = Math.floor(timeDiff/(1000 * 60 * 60 * 24)) + " day"
+   	var months = Math.floor(timeDiff/(1000 * 60 * 60 * 24 * 30)) + " month"
+    var years = Math.floor(timeDiff/(1000 * 60 * 60 * 24 * 30 * 12)) + " year"
+    
+    var timeArray = [years, months, days, hours, minutes, seconds]
+
+    for(var i = 0; i < timeArray.length; i++) {
+    	if (parseInt(timeArray[i]) !== 0) {
+            
+            if(parseInt(timeArray[i]) >= 2) {
+            	return "Updated about " + timeArray[i] + "s ago"
+            }
+            else if (parseInt(timeArray[i]) < 2) {
+                return "Updated about " + timeArray[i] + " ago"
+            }
+        }
+        
+    }
+       
+}
+
+var userPromise = $.getJSON(myFullUserUrl)
 
 var profileDataHandler = function (responseObj) {
 
@@ -83,19 +106,47 @@ var profileDataHandler = function (responseObj) {
 		followers = responseObj.followers,
 		following = responseObj.following,
 		starred_url = responseObj.starred_url
-		console.log(starred_url)
-			if (blog === null) {
-			blog = "not listed"
+
+		if (avatar_url !== null) {
+			htmlString += "<img src = ' " + avatar_url + "'/>"			
+
 		}
 
-		htmlString += "<img src = ' " + avatar_url + "'/>"
-		htmlString += "<h1 id = 'fullName'>" + fullName + "</h1>"
-		htmlString += "<h3 id = 'loginName'>" + login + "</h3>"
-		htmlString += "<p id = 'blog'>" + blog + "</p>"
-		htmlString += "<p id = 'location'>" + location + "</p>"
-		htmlString += "<p id = 'email'>" + email + "</p>"
-		htmlString += "<p id = 'createdAt'> Joined on " + created_at + "</p>" 
-		htmlString += "<p id = 'html_url'>" + html_url + "</p>"
+		htmlString += '<div class = "userInfo">'
+		if (fullName !== null) {
+			htmlString += "<h1 id = 'fullName'>" + fullName + "</h1>"
+
+		}
+		
+
+		if (login !== null) {
+			htmlString += "<h3 id = 'loginName'>" + login + "</h3>"
+
+		}
+		
+		htmlString += '</div>'
+	
+		
+		if (location !== null) {
+			htmlString += '<div>'
+			htmlString += "<i class='fa fa-map-marker' aria-hidden='true'></i><span id = 'location'>" + location + "</span>"
+			htmlString += '</div>'
+		}
+
+		if (blog !== null) {
+			htmlString += '<p id = "blog"><a href="' + blog + '">' + blog + '</a></p>'
+		}
+
+		if (email !== null) {
+			htmlString += '<div>'
+			htmlString += "<i class='fa fa-envelope-o' aria-hidden='true'></i><span id = 'email'><a href=mailto:' + email + '>" + email + "</a></span>"
+			htmlString += '</div>'
+			
+		}
+
+		htmlString += '<div>'
+		htmlString += "<i class='fa fa-clock-o' aria-hidden='true'></i><span id = 'createdAt'> Joined on" + created_at + "</span>" 
+		htmlString += '</div>'
 		htmlString += "<div id = 'followContainer'>"
 		htmlString += "<div id = 'followersContainer'>"
 		htmlString += "<p id = 'followers'>" + followers + "</p>" 
@@ -112,18 +163,37 @@ var profileDataHandler = function (responseObj) {
 		leftContainer.innerHTML = htmlString
 }
 
+userPromise.then(profileDataHandler)
+
+var repoPromise = $.getJSON(myFullRepoUrl)
 		
 var repoDataHandler = function (repoArray) {
 
 	var repoCards = " "
 	for (var i = 0; i < repoArray.length; i++) {
 		var repObj = repoArray[i],
-					name = repObj.name
+					name = repObj.name,
+					repoUrl = repObj.html_url,
+					repoLang = repObj.language,
+					repoForks = repObj.forks_count
+					repoStars = repObj.stargazers_count
+					repoUpdate = repObj.updated_at
+
 
 		repoCards += '<div class="repoCard">'
-		repoCards += '<div class="portrait">' 
-		repoCards += 	'<h2 class="name">' + name + '</h2>'
-		repoCards += '</div>'
+		repoCards += 	'<div class="repo-info">' 
+		repoCards += 		'<a class="name" href="' + repoUrl + '">' + name + '</a>'
+		repoCards += 		'<p class="repo-date">' + updatedLast(repoUpdate) + '</p>'
+		repoCards += 	'</div>'
+		repoCards += 	'<div class="repo-meta">'
+		if(repoLang !== null) {
+			repoCards += '<span>' + repoLang + '</span>'
+
+		}
+
+		repoCards +=		'<span><i class="fa fa-code-fork" aria-hidden="true"></i>' + ' ' + repoForks + '</span>'
+		repoCards +=		'<span><i class="fa fa-star" aria-hidden="true"></i>' + ' ' + repoStars + '</span>'		
+		repoCards +=	'</div>'
 		repoCards += '</div>'
 	}
 	
@@ -131,35 +201,4 @@ var repoDataHandler = function (repoArray) {
 	rightContainer.innerHTML = repoCards
 }
 
-var inputNode = document.querySelector('input[type="text"]')
-
-var handleGitHubSearch = function (eventObj) {
-	// if enter key is pressed 
-	if(eventObj.keyCode === 13) {
-		//Extract value that user typed
-		var inputElement = eventObj.target
-		var inputValue = inputElement.value
-		//console.log("here is the input value>>>>" + inputValue)
-		//make a custom query  with the input value
-		gitHubQuery(inputValue)
-		inputElement.value = ""
-	}
-}
-
-inputNode.addEventListener('keydown', handleGitHubSearch)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+repoPromise.then(repoDataHandler)
